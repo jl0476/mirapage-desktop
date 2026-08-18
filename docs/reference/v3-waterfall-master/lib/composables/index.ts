190ae3ee5e995634cd69bb0c12da75f8a9a9ffd4
jl0476 <jl0476@qq.com>
2026-08-18 17:33:02 +0800

    chore(reference): 删除 v3-waterfall-master 借鉴源码
    3.0.6 瀑布流已开发完成，参考用途已尽，用户主动清理。

--- docs/reference/v3-waterfall-master/lib/composables/index.ts
+++
@@ -1,7 +0,0 @@
-import useUniqueID from './useUniqueId'
-import useColumnsAndTop from './useColumnsAndTop'
-import useLayout from './useLayout'
-import useAnchorObserver from './useAnchorObserver'
-import useVirtualFilter from './useVirtualFilter'
-
-export { useUniqueID, useColumnsAndTop, useLayout, useAnchorObserver, useVirtualFilter }
--- docs/reference/v3-waterfall-master/src/main.ts
+++
@@ -1,6 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import V3waterfall from '../lib'
-
-
-createApp(App).use(V3waterfall).mount('#app')
